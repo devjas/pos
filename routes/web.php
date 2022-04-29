db2eaa9380053bb604ad0efc4eaa9074e265b6c7
devjas <30835414+devjas@users.noreply.github.com>
2022-04-29 16:55:55 -0400

    Create ordering and category controllers and added new features in base.scss

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PosPagesController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PosOrderingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use App\Http\Controllers\PosPagesController;
 Route::get('/', [PosPagesController::class, 'getCategories']);
 Route::get('/items', [PosPagesController::class, 'getItems']);
 Route::get('/addons-extras', [PosPagesController::class, 'getAddonsExtras']);
+
+Route::resource('category', CategoryController::class);
+Route::resource('order', PosOrderingController::class);
