2c16b3c11ffda4a4157ebb48936cc1ac13b52c33
devjas <30835414+devjas@users.noreply.github.com>
2022-05-02 17:22:56 -0400

    Created exit page

---
+++ resources/views/pos/pages/category/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('pos.pos-tpl')
+@section('title') Edit Category @endsection
+
+@section('content')
+<div class="container-fluid pt-4 pb-4">
+	<div class="row justify-content-between">
+		<div class="col-auto"><h4 class="mb-0">Edit Category</h4></div>
+		<div class="col-auto align-self-center">
+			<a href="{{ route('category.index') }}" class="btn pos-bg-red btn-sm text-white"><i class="fas fa-list"></i> My Categories</a>
+		</div>
+	</div>
+	<hr class="mt-1">
+	<div class="row">
+		<div class="col-12 col-md-12 col-lg-8">
+			<div class="card card-body rounded-0 border-0 shadow p-4">
+				<div class="row">
+					<div class="col-12 col-md-8 col-lg-6">
+						<form action="{{ route('category.update', $category->id) }}" method="post">
+							@csrf
+							@method('PUT')
+							<div class="row mb-3">
+								<div class="col-12">
+									<label for="pos_category">CATEGORY NAME</label>
+									<input type="text" name="pos_category" class="form-control border-secondary" value="{{ $category->pos_category }}">
+									@if($errors->has('pos_category'))
+										<span class="color-red fw-bold">{{ $errors->first('pos_category') }}</span>
+									@endif	
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-12">
+									<button type="submit" class="btn btn-lg pos-bg-green text-white">Update Category</button>
+								</div>
+							</div>
+						</form>	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
