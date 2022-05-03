5d5f324bdc5e204fb497716c96834606778aec47
devjas <30835414+devjas@users.noreply.github.com>
2022-05-03 10:44:46 -0400

    Added a warning message. We can now show error, warning and success messages

--- resources/views/pos/partials/_messages.blade.php
+++ resources/views/pos/partials/_messages.blade.php
@@ -1,13 +1,31 @@
 @if(Session::has('success'))
 <div class="container-fluid pt-4">
 	<div class="row">
-		<div class="col-12"><div class="alert alert-success"><i class="fas fa-check"></i> {{ Session::get('success') }}</div></div>
+		<div class="col-12">
+			<div class="alert alert-success">
+				<i class="far fa-smile fs-2 align-middle me-2"></i> <span class="align-middle pt-1 d-inline-block">{{ Session::get('success') }}</span>
+			</div>
+		</div>
 	</div>
 </div>
 @elseif(Session::has('error'))
 <div class="container-fluid pt-4">
 	<div class="row">
-		<div class="col-12"><div class="alert alert-danger"><i class="fas fa-exclamation-triangle"></i> {{ Session::get('error') }}</div></div>
+		<div class="col-12">
+			<div class="alert alert-danger">
+				<i class="far fa-frown fs-2 align-middle me-2"></i> <span class="align-middle pt-1 d-inline-block">{{ Session::get('error') }}</span>
+			</div>
+		</div>
+	</div>
+</div>
+@elseif(Session::has('warning'))
+<div class="container-fluid pt-4">
+	<div class="row">
+		<div class="col-12">
+			<div class="alert alert-warning">
+				<i class="far fa-meh fs-2 align-middle me-2"></i> <span class="align-middle pt-1 d-inline-block">{{ Session::get('warning') }}</span>
+			</div>
+		</div>
 	</div>
 </div>
 @endif
