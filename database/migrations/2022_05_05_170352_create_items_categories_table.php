ef65d8238f3e20512622b86467040f9dce574930
devjas <30835414+devjas@users.noreply.github.com>
2022-05-07 19:16:47 -0400

    New Database Files

---
+++ database/migrations/2022_05_05_170352_create_items_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items_categories');
+    }
+}
