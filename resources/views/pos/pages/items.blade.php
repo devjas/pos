9b49f887aed624fec68c8b3fada64c520c50e3d7
devjas <30835414+devjas@users.noreply.github.com>
2022-05-07 18:48:42 -0400

    Items are now displaying dynamically

--- resources/views/pos/pages/items.blade.php
+++ resources/views/pos/pages/items.blade.php
@@ -7,18 +7,12 @@
 	<div class="row">
 		<div class="col-7">
 			<div class="btn-block-container items">
-				<a href="/addons-extras" class="pos-btn-block shadow-sm">
-					<span class="row h-100"><span class="col-12 align-self-center">{{ __('CHICKEN QUESADILLA') }}</span></span>
-				</a>
-				<a href="/addons-extras" class="pos-btn-block shadow-sm">
-					<span class="row h-100"><span class="col-12 align-self-center">{{ __('CLAMS CASINO') }}</span></span>
-				</a>
-				<a href="/addons-extras" class="pos-btn-block shadow-sm">
-					<span class="row h-100"><span class="col-12 align-self-center">{{ __('GARLIC BREAD SUPREME') }}</span></span>
-				</a>
-				<a href="/addons-extras" class="pos-btn-block shadow-sm">
-					<span class="row h-100"><span class="col-12 align-self-center">{{ __('FRIED CALAMARY') }}</span></span>
-				</a>
+				@foreach($items as $item)
+					<a href="/addons-extras" class="pos-btn-block shadow-sm">
+						<span class="row h-100"><span class="col-12 align-self-center">{{ $item->item_name }}</span></span>
+					</a>
+				@endforeach
+				
 			</div>
 		</div>
 		<div class="col-5">@include('pos.modules.current-orders')</div>
