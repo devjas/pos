b1e5e07ccef0e4b1acb8115554aa34d00d376a6d
devjas <30835414+devjas@users.noreply.github.com>
2022-05-27 07:10:04 -0400

    New controllers added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\PosPagesController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\PosOrderingController;
+use App\Http\Controllers\AddonController;
+use App\Http\Controllers\ItemsAddonsController;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +24,12 @@ Route::get('/', [PosPagesController::class, 'getCategories']);
 Route::get('/items/{id}', [PosPagesController::class, 'getItems']);
 Route::get('/addons-extras', [PosPagesController::class, 'getAddonsExtras']);
 
-Route::resource('category', CategoryController::class);
-Route::resource('item', ItemController::class);
-Route::resource('order', PosOrderingController::class);
+Route::resources([
+	'category' => CategoryController::class,
+	'item' => ItemController::class,
+	'addon' => AddonController::class,
+	'order' => PosOrderingController::class,
+	'item-addon' => ItemsAddonsController::class
+]);
+
+Route::get('test', [TestController::class, 'test']);
