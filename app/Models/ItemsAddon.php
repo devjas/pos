8b6bb465a5f3f59f0a94d0d99ccbff0f46a36850
devjas <30835414+devjas@users.noreply.github.com>
2022-05-27 07:19:41 -0400

    Addon Models added

---
+++ app/Models/ItemsAddon.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemsAddon extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['item_id', 'addon_id', 'addon_price'];
+}
